d76639f2b9949a53c0ea8ff9ee4449cb66ff50e6
noda takanori <noda@MacBook-Pro.local>
2020-08-13 14:51:37 +0900

    props test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from "react";
 import { render } from "@testing-library/react";
 
-class App extends Component {
-  render() {
-    return (
-      <h1 className="App">
-        <Cat />
-      </h1>
-    );
-  }
-}
+const App = () => {
+  const profiles = [
+    { name: "taro", age: 10 },
+    { name: "hanako", age: 20 },
+    { name: "unknown" },
+  ];
 
-const Cat = () => {
-  return <div>Meow</div>;
+  return (
+    <h1 className="App">
+      {profiles.map((profile, index) => {
+        return <User key={index} name={profile.name} age={profile.age} />;
+      })}
+    </h1>
+  );
+};
+
+const User = (props) => {
+  return (
+    <div>
+      Hi, Iam {props.name}, age{props.age}
+    </div>
+  );
+};
+
+User.defaultProps = {
+  age: 1,
 };
 
 export default App;
