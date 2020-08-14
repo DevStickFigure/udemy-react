d5ec8c4bd30356c7c483f624eeeec67cd1a96a84
noda takanori <noda@MacBook-Pro.local>
2020-08-15 02:17:45 +0900

    add action

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+export const increment = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+
+export const decrement = () => {
+  return {
+    type: DECREMENT,
+  };
+};
