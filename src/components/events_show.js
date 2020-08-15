d10b93fd3cb5dbf91f1e575bc72bc61046caf6e1
noda takanori <noda@MacBook-Pro.local>
2020-08-15 16:30:18 +0900

    add detail page

---
+++ src/components/events_show.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+
+import { getEvent, deleteEvent, putEvent } from '../actions';
+import { Link } from 'react-router-dom';
+
+class EventsShow extends Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  renderField(field) {
+    const {
+      input,
+      label,
+      type,
+      meta: { error },
+    } = field;
+
+    return (
+      <div>
+        <input {...input} placeholder={label} type={type} />
+        {error && <span>{error}</span>}
+      </div>
+    );
+  }
+
+  async onSubmit(values) {
+    //await this.props.postEvent(values);
+    this.props.history.push('/');
+  }
+
+  render() {
+    const { handleSubmit, pristine, submitting } = this.props;
+    return (
+      <div>
+        <form onSubmit={handleSubmit(this.onSubmit)}>
+          <div>
+            <Field label="title" name="title" type="text" component={this.renderField} />
+            <Field label="body" name="body" type="text" component={this.renderField} />
+          </div>
+
+          <div>
+            <input type="submit" value="Submit" disabled={pristine || submitting} />
+            <Link to="/">Cancel</Link>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.title) errors.title = 'Enter Title please.';
+  if (!values.body) errors.body = 'Enter Body please.';
+
+  return errors;
+};
+
+//const mapDispatchToProps = { postEvent };
+
+export default connect(null, null)(reduxForm({ validate, form: 'eventShowForm' })(EventsShow));
